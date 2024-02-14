01836ac3407f923731561863a9e3a4eef0f8542e
jt-frontender <jameswt@live.com>
2024-02-14 12:35:09 +0900

    add new link

--- src/components/Bottomnavbar.js
+++ src/components/Bottomnavbar.js
@@ -28,6 +28,7 @@ const Bottomnavbar = () => {
         <div className="mt-2 underlined cursor-pointer">- Host an online experience </div>
         <div className="mt-2 underlined cursor-pointer">- Community center</div>
         <div className="mt-2 underlined cursor-pointer">- Community center 2</div>
+        <div className="mt-2 underlined cursor-pointer">- Community center 3</div>
       </div>
 
     </div>
