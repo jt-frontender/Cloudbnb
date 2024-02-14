2a3df4090f7dc482f6ca36386e3253cce1d9cc40
jt-frontender <62992336+jt-frontender@users.noreply.github.com>
2024-02-14 12:26:02 +0900

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
 
          <div 
          className="bg-white w-5/12 mx-auto rounded-lg p-5 shadow-2xl md:w-3/12 border border-gray-800 cursor-pointer underlined"
-         >Check availability
+         >Check availability 2
          </div>
 
       </div>
