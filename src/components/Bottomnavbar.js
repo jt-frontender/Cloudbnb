177cd12fd3c3ccfd0f32575cdaed747d684402ad
jt-frontender <jameswt@live.com>
2024-02-14 12:21:59 +0900

    add test

--- src/components/Bottomnavbar.js
+++ src/components/Bottomnavbar.js
@@ -27,6 +27,8 @@ const Bottomnavbar = () => {
         <div className="mt-2 underlined cursor-pointer">- Host an experience</div>
         <div className="mt-2 underlined cursor-pointer">- Host an online experience </div>
         <div className="mt-2 underlined cursor-pointer">- Community center</div>
+        <div className="mt-2 underlined cursor-pointer">1</div>
+        <div className="mt-2 underlined cursor-pointer">2</div>
       </div>
 
     </div>
